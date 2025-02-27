2db1850c85709822fd9246111f1462d4f752613f
서민정 <ybsm0629@gmail.com>
2025-02-27 17:12:06 +0900

    수업일지 목록 정리

--- src/main/java/com/thirdproject/controller/LessonLogController.java
+++ src/main/java/com/thirdproject/controller/LessonLogController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.thirdproject.repository.vo.LessonLogVo;
 import com.thirdproject.repository.vo.UserVo;
@@ -108,5 +109,7 @@ public class LessonLogController {
         logger.debug("LESSONLOG DELETE:" + no);
         lessonLogServiceImpl.deleteLessonLog(no);
         return "redirect:/lesson/list";
-    }
+    }    
+    
+    
 }
